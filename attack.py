ea4b278ff1517469ff774bb2f0c12639a5659ae5
JokerYan <770792058@qq.com>
2021-07-22 15:06:00 +0800

    leave momentum unused

--- attack.py
+++ attack.py
@@ -30,7 +30,7 @@ from pytorch_image_classification.utils import (
 from utils.debug_tools import clear_debug_image, save_image_stack
 
 # attack parameters temporarily attached here
-c = 0.3
+c = 3
 lr = 0.1
 momentum = 0.9
 steps = 100
