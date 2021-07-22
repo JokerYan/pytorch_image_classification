ded97de6da074097fcaa527e634b05db9cf2f456
JokerYan <770792058@qq.com>
2021-07-22 15:14:57 +0800

    remove tqdm for attack

--- attack.py
+++ attack.py
@@ -31,7 +31,7 @@ from utils.debug_tools import clear_debug_image, save_image_stack
 
 # attack parameters temporarily attached here
 c = 3
-lr = 0.01
+lr = 0.03
 momentum = 0.9
 steps = 100
 batch_size = 1
