5a44a6af890c0e8c188a8fc3e17c267841c7f4d7
JokerYan <770792058@qq.com>
2021-07-24 22:20:14 +0800

    change attack c

--- attack.py
+++ attack.py
@@ -30,7 +30,7 @@ from pytorch_image_classification.utils import (
 from utils.debug_tools import clear_debug_image, save_image_stack
 
 # attack parameters temporarily attached here
-c = 1
+c = 5
 lr = 0.05
 momentum = 0.9
 steps = 200
