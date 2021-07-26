cdb7f685320171bc1da2c576ca7df87ee66f000a
JokerYan <770792058@qq.com>
2021-07-26 22:07:50 +0800

    adjust gaussian std

--- custom_models/smooth_model.py
+++ custom_models/smooth_model.py
@@ -5,7 +5,7 @@ from utils.debug_tools import save_image_stack, clear_debug_image
 
 
 class SmoothModel(nn.Module):
-    def __init__(self, base_model, mean=0, std=0.1, sample_size=10):
+    def __init__(self, base_model, mean=0, std=0.3, sample_size=10):
         super(SmoothModel, self).__init__()
         self.base_model = base_model
         self.mean = mean
@@ -20,7 +20,8 @@ class SmoothModel(nn.Module):
         for i in range(self.sample_size):
             gaussian_noise = torch.normal(self.mean * input_dummy, self.std * input_dummy).cuda()
             gaussian_input = x + gaussian_noise
-            save_image_stack(gaussian_input, "gaussian_input", normalized=True)
+            save_image_stack(gaussian_input, "gaussian_input_{}".format(i), normalized=True)
+            save_image_stack(x, "normal_input_{}".format(i), normalized=True)
             gaussian_output = self.base_model(gaussian_input)
             output_list.append(gaussian_output)
         return torch.mean(torch.stack(output_list), dim=0)
