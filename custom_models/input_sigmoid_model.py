94e4d24e7fd77dd3f76a957e1dc6e8e96637dee2
JokerYan <770792058@qq.com>
2021-07-28 22:12:40 +0800

    reduce temperature to 0.1

--- custom_models/input_sigmoid_model.py
+++ custom_models/input_sigmoid_model.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 class Network(nn.Module):
     def __init__(self, config, base_model):
         super(Network, self).__init__()
-        self.temperature = 0.3
+        self.temperature = 0.1
         self.config = config
         self.base_model = base_model
 
