d61ba8ee947ce9a14dd675f79b06c5e8cd3c035a
JokerYan <770792058@qq.com>
2021-07-29 23:03:00 +0800

    remove retain graph

--- custom_loss/llv_loss.py
+++ custom_loss/llv_loss.py
@@ -11,7 +11,8 @@ class LocalLipschitzValueLoss:
 
         assert model_input.requires_grad
         max_output = torch.sum(torch.max(output, dim=1).values)
-        max_output.backward(retain_graph=True)
+        # max_output.backward(retain_graph=True)
+        max_output.backward()
         # input_grad = model_input.grad.data
         # input_grad_norm = torch.norm(input_grad, p=2)  # l2 norm
         # print("base_loss: {}\tgrad_norm: {}".format(base_loss, torch.max(input_grad)))
