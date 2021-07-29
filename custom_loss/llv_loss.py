91d077022e556e9bf4ffbd517ce4b420ca9dfba9
JokerYan <770792058@qq.com>
2021-07-29 23:52:09 +0800

    add logger to llv loss

--- custom_loss/llv_loss.py
+++ custom_loss/llv_loss.py
@@ -3,9 +3,10 @@ import torch.nn as nn
 
 
 class LocalLipschitzValueLoss:
-    def __init__(self, base_loss_func):
+    def __init__(self, base_loss_func, logger=None):
         self.base_loss_func = base_loss_func
         self.norm_ratio = 1
+        self.logger = logger
 
     def __call__(self, output, target, model_input):
         base_loss = self.base_loss_func(output, target)
@@ -16,7 +17,11 @@ class LocalLipschitzValueLoss:
         # input_grad = model_input.grad.data
         input_grad = torch.autograd.grad(max_output, model_input, retain_graph=True, create_graph=True)[0]
         input_grad_norm = torch.norm(input_grad, p=2)  # l2 norm
-        print("base_loss: {}\tgrad_norm: {}".format(base_loss, torch.max(input_grad)))
+        msg = "base_loss: {}\tgrad_norm: {}".format(base_loss, torch.max(input_grad))
+        if self.logger is not None:
+            self.logger.info(msg)
+        else:
+            print(msg)
 
         total_loss = base_loss + self.norm_ratio * input_grad_norm
 
