7a03462e839ac188ee373a021fc99da22d33c82f
JokerYan <770792058@qq.com>
2021-07-29 23:01:25 +0800

    backward on max_output

--- custom_loss/llv_loss.py
+++ custom_loss/llv_loss.py
@@ -9,9 +9,9 @@ class LocalLipschitzValueLoss:
     def __call__(self, output, target, model_input):
         base_loss = self.base_loss_func(output, target)
 
-        # assert model_input.requires_grad
-        # max_output = torch.sum(torch.max(output, dim=1).values)
-        # max_output.backward(retain_graph=True)
+        assert model_input.requires_grad
+        max_output = torch.sum(torch.max(output, dim=1).values)
+        max_output.backward(retain_graph=True)
         # input_grad = model_input.grad.data
         # input_grad_norm = torch.norm(input_grad, p=2)  # l2 norm
         # print("base_loss: {}\tgrad_norm: {}".format(base_loss, torch.max(input_grad)))
