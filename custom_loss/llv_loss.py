a2dff9bf2cb7859044fff732b59ea53a86616938
JokerYan <770792058@qq.com>
2021-07-29 23:50:18 +0800

    use llv total loss

--- custom_loss/llv_loss.py
+++ custom_loss/llv_loss.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 class LocalLipschitzValueLoss:
     def __init__(self, base_loss_func):
         self.base_loss_func = base_loss_func
+        self.norm_ratio = 1
 
     def __call__(self, output, target, model_input):
         base_loss = self.base_loss_func(output, target)
@@ -16,7 +17,7 @@ class LocalLipschitzValueLoss:
         input_grad = torch.autograd.grad(max_output, model_input, retain_graph=True, create_graph=True)[0]
         input_grad_norm = torch.norm(input_grad, p=2)  # l2 norm
         print("base_loss: {}\tgrad_norm: {}".format(base_loss, torch.max(input_grad)))
-        # input_grad_norm.requires_grad = True
-        debug_grad = torch.autograd.grad(input_grad_norm, model_input, retain_graph=True)[0]
 
-        return base_loss
+        total_loss = base_loss + self.norm_ratio * input_grad_norm
+
+        return total_loss
