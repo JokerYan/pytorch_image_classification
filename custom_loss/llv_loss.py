917b148703d4b933a28fe9c74dec6a671d295778
JokerYan <770792058@qq.com>
2021-08-02 22:51:00 +0800

    change llv loss from max_output_grad to loss grad

--- custom_loss/llv_loss.py
+++ custom_loss/llv_loss.py
@@ -15,7 +15,8 @@ class LocalLipschitzValueLoss:
 
         # max_output.backward(retain_graph=False, create_graph=True)
         # input_grad = model_input.grad.data
-        input_grad_norm = self.get_input_grad_norm(output, model_input)
+        # input_grad_norm = self.get_input_grad_norm(output, model_input)
+        input_grad_norm = self.get_input_loss_grad_norm(model_input, base_loss)
         total_loss = base_loss + self.norm_ratio * torch.relu(input_grad_norm - self.llv_thresh)
 
         msg = "base_loss: {}\tgrad_norm: {}\ttotal_loss: {}".format(base_loss, input_grad_norm, total_loss)
@@ -33,6 +34,14 @@ class LocalLipschitzValueLoss:
         max_output = torch.sum(torch.max(output, dim=1).values)
         input_grad = torch.autograd.grad(max_output, model_input, retain_graph=is_train, create_graph=is_train)[0]
         input_grad_norm = torch.norm(input_grad, p=2)  # l2 norm
+
+        return input_grad_norm
+
+    @staticmethod
+    def get_input_loss_grad_norm(model_input, base_loss):
+        assert model_input.requires_grad
+        input_grad = torch.autograd.grad(base_loss, model_input, retain_graph=True, create_graph=True)[0]
+        input_grad_norm = torch.norm(input_grad, p=2)
 
         return input_grad_norm
 
