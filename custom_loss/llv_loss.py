453a55c532dc26922c9f1fc6df829f357c1956aa
JokerYan <770792058@qq.com>
2021-08-03 13:21:49 +0800

    debug change norm_ratio to 0

--- custom_loss/llv_loss.py
+++ custom_loss/llv_loss.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 class LocalLipschitzValueLoss:
     def __init__(self, base_loss_func, logger=None):
         self.base_loss_func = base_loss_func
-        self.norm_ratio = 0.1
+        self.norm_ratio = 0
         self.logger = logger
 
         self.llv_thresh = 0
