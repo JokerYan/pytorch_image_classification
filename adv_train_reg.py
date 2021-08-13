8c95b0c9ed3ea4e23a2704919110075aeba26ba0
JokerYan <770792058@qq.com>
2021-08-13 10:50:44 +0800

    fix normal_output typo

--- adv_train_reg.py
+++ adv_train_reg.py
@@ -152,7 +152,7 @@ def train(epoch, config, model, optimizer, scheduler, loss_func, train_loader,
         noise_inputs.requires_grad = False
         adv_inputs.requires_grad = False
 
-        natural_loss = loss_func(normal_output, targets)
+        natural_loss = loss_func(normal_outputs, targets)
         robust_loss = (1.0 / batch_size) * criterion_kl(torch.log_softmax(adv_outputs, dim=1),
                                                         torch.softmax(normal_outputs, dim=1))
         loss = natural_loss + 6 * robust_loss
