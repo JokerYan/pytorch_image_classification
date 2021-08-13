8f9c54c28b62edd6a84b6a0974b411f003d3c860
JokerYan <770792058@qq.com>
2021-08-13 21:35:20 +0800

    debug print loss

--- torchattack_post.py
+++ torchattack_post.py
@@ -168,7 +168,7 @@ def post_tune(config, model, images):
 
             loss = loss_func(noise_outputs, targets)  # loss to be maximized
             input_grad = torch.autograd.grad(loss, noise_inputs)[0]
-            print(torch.mean(torch.abs(input_grad)))
+            # print(torch.mean(torch.abs(input_grad)))
             delta = noise + alpha * torch.sign(input_grad)
             delta.clamp_(-epsilon, epsilon)
 
@@ -180,6 +180,7 @@ def post_tune(config, model, images):
 
             optimizer.zero_grad()
             loss = loss_func(outputs, targets)
+            print(loss)
             loss.backward()
             optimizer.step()
 
