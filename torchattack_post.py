52a943465e7396b2a2c447ec0f61a1be057fbe7c
JokerYan <770792058@qq.com>
2021-08-13 20:41:06 +0800

    roll targets only once

--- torchattack_post.py
+++ torchattack_post.py
@@ -156,8 +156,8 @@ def post_tune(config, model, images):
     with torch.enable_grad():
         model = copy.deepcopy(model)
         optimizer = create_optimizer(config, model)
+        targets = torch.randint(0, 9, [len(images)]).to(device)
         for i in range(100):
-            targets = torch.randint(0, 9, [len(images)]).to(device)
             delta = (torch.rand_like(images) * 2 - 1) * epsilon  # uniform rand from [-eps, eps]
             noise_inputs = images + delta
             noise_inputs.requires_grad = True
