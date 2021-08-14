f97f94879f2f277c2e157b5f0faa2312ff9508db
JokerYan <770792058@qq.com>
2021-08-14 15:25:39 +0800

    reduce r

--- torchattack_post.py
+++ torchattack_post.py
@@ -184,7 +184,7 @@ def post_tune(config, model, images):
     original_output = fix_model(images)
     with torch.enable_grad():
         # optimizer = create_optimizer(config, model)
-        optimizer = torch.optim.SGD(lr=0.001,
+        optimizer = torch.optim.SGD(lr=0.0001,
                                     params=model.parameters(),
                                     momentum=config.train.momentum,
                                     nesterov=config.train.nesterov)
