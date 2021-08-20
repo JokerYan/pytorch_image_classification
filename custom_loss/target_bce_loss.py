c5d991b85f3e27db7efb1db943a99a773b02338c
JokerYan <770792058@qq.com>
2021-08-20 20:56:06 +0800

    debug print binary target

--- custom_loss/target_bce_loss.py
+++ custom_loss/target_bce_loss.py
@@ -10,26 +10,20 @@ class TargetBCELoss:
         first_output = output[:, first_class]  # batch_size x 1
         second_output = output[:, second_class]  # batch_size x 1
 
-        binary_output = torch.hstack([
-            torch.unsqueeze(first_output, 0),
-            torch.unsqueeze(second_output, 0)]
-        )
-        binary_output = torch.hstack([
-            first_output,
-            second_output]
-        )
+        binary_output = torch.hstack([first_output, second_output])
         binary_softmax_output = torch.softmax(binary_output, dim=1)
         binary_first_class_output = binary_softmax_output[:, 0]
 
         ones = torch.ones_like(label)
         zeros = torch.zeros_like(label)
         binary_target = torch.where(label == int(first_class), ones, zeros)
-
-        print(first_output.shape)
-        print(binary_output.shape)
-        print(label)
-        print(binary_softmax_output)
-        print(first_class, second_class)
+        #
+        # print(first_output.shape)
+        # print(binary_output.shape)
+        # print(label)
+        # print(binary_softmax_output)
+        # print(first_class, second_class)
+        print(binary_target)
 
         loss = self.bce_loss(binary_first_class_output, binary_target)
 
