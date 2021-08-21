afb3ccd4e1b5eb01a5406bd8cfbdf2c32b0ea42c
JokerYan <770792058@qq.com>
2021-08-21 16:19:34 +0800

    use softmax in blloss

--- custom_loss/target_bce_loss.py
+++ custom_loss/target_bce_loss.py
@@ -29,12 +29,12 @@ class TargetBLLoss:
         second_output = output[:, second_class]  # batch_size x 1
 
         binary_output = torch.hstack([first_output, second_output])
-        # binary_softmax_output = torch.softmax(binary_output, dim=1)
+        binary_softmax_output = torch.softmax(binary_output, dim=1)
         # binary_first_class_output = binary_softmax_output[:, 0]
 
         # ones = torch.ones_like(label)
         # zeros = torch.zeros_like(label)
         # binary_target = torch.where(label == int(first_class), ones, zeros).float()
 
-        loss = torch.mean(binary_output[:, 0] - binary_output[:, 1])
+        loss = torch.mean(binary_softmax_output[:, 0] - binary_softmax_output[:, 1])
         return loss
