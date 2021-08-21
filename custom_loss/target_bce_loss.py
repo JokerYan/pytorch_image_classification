e1ba0d28d7f6e58e00a0512a2b1c24bcf4c68aa3
JokerYan <770792058@qq.com>
2021-08-21 16:28:19 +0800

    implement output difference in blloss

--- custom_loss/target_bce_loss.py
+++ custom_loss/target_bce_loss.py
@@ -24,20 +24,18 @@ class TargetBCELoss:
 
 # binary linear loss
 class TargetBLLoss:
-    def __init__(self):
-        self.mse_loss = MSELoss()
-
     def __call__(self, output, label, first_class, second_class):
         first_output = output[:, first_class]  # batch_size x 1
         second_output = output[:, second_class]  # batch_size x 1
 
         binary_output = torch.hstack([first_output, second_output])
-        binary_softmax_output = torch.softmax(binary_output, dim=1)
+        # binary_softmax_output = torch.softmax(binary_output, dim=1)
+        binary_softmax_output = binary_output
         binary_first_class_output = binary_softmax_output[:, 0]
+        binary_second_class_output = binary_softmax_output[:, 1]
+        output_difference = binary_first_class_output - binary_second_class_output
 
-        ones = torch.ones_like(label)
-        zeros = torch.zeros_like(label)
-        binary_target = torch.where(label == int(first_class), ones, zeros).float()
+        loss_tensor = torch.where(label == int(first_class), -1 * output_difference, output_difference).float()
 
-        loss = self.mse_loss(binary_first_class_output, binary_target)
+        loss = torch.mean(loss_tensor)
         return loss
