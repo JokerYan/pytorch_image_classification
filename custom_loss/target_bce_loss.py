0ac214a484eeb4d0acef9fea6719c7ded2894d88
JokerYan <770792058@qq.com>
2021-08-21 16:23:54 +0800

    use mse loss in bllloss

--- custom_loss/target_bce_loss.py
+++ custom_loss/target_bce_loss.py
@@ -1,5 +1,5 @@
 import torch
-from torch.nn import BCELoss
+from torch.nn import BCELoss, MSELoss
 
 
 class TargetBCELoss:
@@ -24,17 +24,20 @@ class TargetBCELoss:
 
 # binary linear loss
 class TargetBLLoss:
+    def __init__(self):
+        self.mse_loss = MSELoss()
+
     def __call__(self, output, label, first_class, second_class):
         first_output = output[:, first_class]  # batch_size x 1
         second_output = output[:, second_class]  # batch_size x 1
 
         binary_output = torch.hstack([first_output, second_output])
         binary_softmax_output = torch.softmax(binary_output, dim=1)
-        # binary_first_class_output = binary_softmax_output[:, 0]
+        binary_first_class_output = binary_softmax_output[:, 0]
 
-        # ones = torch.ones_like(label)
-        # zeros = torch.zeros_like(label)
-        # binary_target = torch.where(label == int(first_class), ones, zeros).float()
+        ones = torch.ones_like(label)
+        zeros = torch.zeros_like(label)
+        binary_target = torch.where(label == int(first_class), ones, zeros).float()
 
-        loss = torch.mean(binary_softmax_output[:, 0] - binary_softmax_output[:, 1])
+        loss = self.mse_loss(binary_first_class_output, binary_target)
         return loss
